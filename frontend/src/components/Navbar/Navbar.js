89df393169a53ae0848a8df84e1cb6ceb2ad3789
Hayden <haydenhornsby@hotmail.com>
2023-02-02 18:22:35 +0000

    Added and set up component styles for Navbar

---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from "./Navbar.scss"
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+        </div>
+    );
+};
+
+export default Navbar;
