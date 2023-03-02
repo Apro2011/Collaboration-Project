55e40f3cc11252c6072b8c7aa23df25f8dc2cf5b
monire <aglida1370@gmail.com>
2023-03-02 21:06:25 +0330

    Handle logout

--- frontend/src/components/NavList/NavList.jsx
+++ frontend/src/components/NavList/NavList.jsx
@@ -1,15 +1,23 @@
+import { Router } from 'next/router';
 import React from 'react';
+import { useRouter } from 'next/router'
 
 import styles from './NavList.module.css';
 
 function NavList({loggedIn}) {
+    const router = useRouter()
+    const handleLoguout = ()=>{
+        localStorage.clear()
+        router.push('/sign-in')
+    }
+
     return (
         <ul className={styles['navlist']}>
             {loggedIn === true && <li className={styles['navlist__item']}>
                 <a className={styles['navlist__link']} href='#'>Create Blog Post</a>
             </li>}
-            <li className={styles['navlist__item']}>
-                <a className={styles['navlist__link']} href='#'>{loggedIn=== true ? 'Log Out' : 'Log In'}</a>
+            <li className={styles['navlist__item']} onClick={handleLoguout}>
+                <span className={styles['navlist__link']}>{loggedIn=== true ? 'Log Out' : 'Log In'}</span>
             </li>
         </ul>
     );
