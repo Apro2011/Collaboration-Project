bb90dc0214022f2aa227f9b2d47c21d9eaab4d9d
Richard Howell <yo@richardhowell.dev>
2023-03-10 02:09:30 +0000

    Create Storybook component structure and add example button component

--- design-system/.storybook/main.js
+++ design-system/.storybook/main.js
@@ -1,12 +1,15 @@
 module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../components/**/*.stories.mdx",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "storybook-css-modules",
   ],
   "framework": "@storybook/react",
   webpackFinal: async (config) => {
